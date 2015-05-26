9e955ba98a502347af75ad480de9768fdf2eb388
wcornwell <wcornwell@gmail.com>
2015-05-26 11:25:39 +1000

    more elegant overlap function

--- overlap.R
+++ overlap.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(Hmisc)
 library(ape)
+library(data.table)
 source("data_manipulation_functions.R")
 
 # this is the genbank species list from the NCBI Browser website
@@ -34,25 +35,31 @@ tree$tip.label <- gsub("_", " ", tree$tip.label)
 tree.names<-scrub(tree$tip.label)
 tree.names<-use.synonym.lookup(tree.names)
 
+library(data.table)
+fread("species_centers.txt")%>%
+  select(sp=V2)%>%
+  mutate(sp=scrub(sp))%>%
+  mutate(sp=use.synonym.lookup(sp))->gbif.all
+
+
 #clean up a bit
 rm(list=c("try.all","tree","genbank.2","genbank.all"))
 gc()   
 
-#if i were a better programer these two would be one function
-calculate.overlap<-function(y){
-  pw<-combn(y,2,FUN=function(x)sum(x[[1]]%in%x[[2]]))
-  names(pw)<-combn(y,2,FUN=function(x)paste(names(x)[[1]],names(x)[[2]],sep="-"))
-  return(pw)
+#thanks to dudes on stackoverflow
+calculate.overlap <- function(y, i){
+  pw <- combn(seq_along(y), i, FUN= function(x) {
+    res <- length(Reduce(intersect, y[x]))
+    names(res) <- paste(names(y[x]), collapse = "-")
+    res
+  }, simplify = FALSE)
+  return(do.call(c, pw))
 }
-calculate.overlap.3<-function(y){
-  pw<-combn(y,3,FUN=function(x)sum(x[[1]]%in%x[[2]]&x[[1]]%in%x[[3]]))
-  names(pw)<-combn(y,3,FUN=function(x)paste(names(x)[[1]],names(x)[[2]],names(x)[[3]],sep="-"))
-  return(pw)
-}
+
 
 
 y<-list(zanne.tree=tree.names,try.all.names=try.sp,genbank.parse1=genbank,genbank.parse2=genbank2,plant.list.1.1=pl)
-out<-c(sapply(y,length),calculate.overlap(y),calculate.overlap.3(y))
+out<-c(sapply(y,length),calculate.overlap(y,2),calculate.overlap(y,3))
 write.csv(out,"output/two_and_three_way_comparisons.csv")
 
 #FOR FUTURE CHECKS:
