b20d78e0017573e13a32311dfcbb805427dd1680
wcornwell <wcornwell@gmail.com>
2016-01-10 18:20:30 +1100

    some progress

--- R/overlap.R
+++ R/overlap.R
@@ -56,7 +56,7 @@ do_overlap_analysis<-function(){
 y<-list(genbank=get_genbank(),
         try.all.names=read.in.try(),
         gbif=get_gbif_names(),
-        zae=read.tree("zanne1.1.tree")$tip.labels)
+        zae=read.tree("zanne1.1.tre")$tip.labels)
 out<-c(sapply(y,length),calculate.overlap(y,2),calculate.overlap(y,3))
 write.csv(out,"tables/two_and_three_way_comparisons.csv")
 }
